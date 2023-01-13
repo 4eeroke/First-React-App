328bc29f891bea634165fcd6265069ef4f72a68f
Усманов Реналь Линарович <251918@mck-ktits.ru>
2023-01-13 14:34:41 +0300

    React App2

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,16 @@
-function Header() {
+// function Header() {
+//     return (
+//         <header>
+//             <h1>Header</h1>
+//         </header>
+//     );
+// }
+
+// export default Header;
+
+
+
+const Header = () => {
     return (
         <header>
             <h1>Header</h1>
